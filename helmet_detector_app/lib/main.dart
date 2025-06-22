9cfe95f9ac47022a790c9c1d94598b576a08bfdd
Fahrel <fahrelazki@gmail.com>
2025-06-22 13:42:22 +0300

    update

--- helmet_detector_app/lib/main.dart
+++ helmet_detector_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:helmet_detector_app/main_screen.dart';
+import 'package:helmet_detector_app/screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
