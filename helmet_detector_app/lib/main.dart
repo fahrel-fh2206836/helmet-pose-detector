739314b4bd13090cdf119b4f033d7a9b55b64acc
Fahrel <fahrelazki@gmail.com>
2026-01-04 16:46:46 +0300

    trials conducted

--- helmet_detector_app/lib/main.dart
+++ helmet_detector_app/lib/main.dart
@@ -8,7 +8,7 @@ import 'package:helmet_detector_app/services/permission_service.dart';
 // Uncomment to benchmark
 // import 'package:helmet_detector_app/benchmark/benchmark_classifer_v1.dart';
 // import 'package:helmet_detector_app/benchmark/benchmark_classifier_with_csv_v2.dart';
-import 'package:helmet_detector_app/benchmark/benchmark_helmet_detection.dart';
+// import 'package:helmet_detector_app/benchmark/benchmark_helmet_detection.dart';
 
 // Note: App currently working only on Android phones.
 
@@ -29,30 +29,30 @@ void main() async {
   //   selectDelegate: "XNNPACK",
   // );
 
-  await runYoloInt8NmsBenchmarkIsolatedWithCSV(
-    deviceName: "S24+",
-    modelLabel: "YOLOv11n INT8 640x640",
-    modelAsset: "assets/int8/best_full_integer_quant.tflite",
-    selectDelegate: "XNNPACK",
-    threads: const [4],
-    runs: 100,
-    warmup: 10,
-    imgSize: 320,
-  );
+  // await runYoloInt8NmsBenchmarkIsolatedWithCSV(
+  //   deviceName: "S24+",
+  //   modelLabel: "YOLOv11n INT8 640x640",
+  //   modelAsset: "assets/int8/best_full_integer_quant.tflite",
+  //   selectDelegate: "XNNPACK",
+  //   threads: const [4],
+  //   runs: 100,
+  //   warmup: 10,
+  //   imgSize: 320,
+  // );
 
   // Prevents Landscape orientation
-  // await SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp, // Lock to upright portrait
-  //   DeviceOrientation.portraitDown, // Allow upside-down portrait
-  // ]);
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, // Lock to upright portrait
+    DeviceOrientation.portraitDown, // Allow upside-down portrait
+  ]);
 
-  // // Requests all necesary permissions
-  // await PermissionService.requestAllPermissions();
+  // Requests all necesary permissions
+  await PermissionService.requestAllPermissions();
 
-  // // Initializes notifcation services
-  // await NotiService().initNotification();
+  // Initializes notifcation services
+  await NotiService().initNotification();
 
-  // runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
